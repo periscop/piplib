b505da9dddd9e7aca936dee0c438da596ea87709
Sven Verdoolaege <skimo@kotnet.org>
2006-01-20 16:45:01 +0100

    example: allow specification of options

--- example/example.c
+++ example/example.c
@@ -4,14 +4,44 @@
  * compare results given by PIP by typing 'pip32 FILE.dat'
  */
 
-# include <stdio.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 #include <piplib/piplib.h>
 
-int main()
+static PipOptions *options_read(FILE *f)
+{
+  char s[1024];
+  PipOptions *options = pip_options_init();
+  while (fgets(s, 1024, f)) {
+    if (strncasecmp(s, "Maximize", 8) == 0)
+      options->Maximize = 1;
+    if (strncasecmp(s, "Urs_parms", 9) == 0)
+      options->Urs_parms = 1;
+    if (strncasecmp(s, "Urs_unknowns", 12) == 0)
+      options->Urs_unknowns = 1;
+  }
+  return options;
+}
+
+int main(int argc, const char **argv)
 { int bignum ;
   PipMatrix  * domain, * context  ;
   PipQuast   * solution ;
   PipOptions * options ;
+  int verbose = 0;
+
+  while (argc > 1) {
+    if (strncmp(argv[1], "-v", 2) == 0) {
+      const char *v = argv[1]+2;
+      ++verbose;
+      while (*v++ == 'v')
+	++verbose;
+    } else
+      break;
+    ++argv;
+    --argc;
+  }
   
   printf("[PIP2-like future input] Please enter:\n- the context matrix,\n") ;
   context = pip_matrix_read(stdin) ;
@@ -26,8 +56,12 @@ int main()
   pip_matrix_print(stdout,domain) ;
   printf("\n") ;
   
-  options = pip_options_init() ;
-  /*options->Maximize = 1 ;*/
+  if (isatty(0))
+    printf("- options (EOF to stop).\n") ;
+  options = options_read(stdin);
+  options->Verbose = verbose;
+  if (isatty(0))
+    pip_options_print(stdout, options);
 
   /* The bignum in PIP1 is fixed on the constraint matrix, here is
    * the translation.
